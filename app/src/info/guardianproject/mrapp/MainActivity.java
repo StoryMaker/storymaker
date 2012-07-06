648e7cc9407dc36d539d8b7c811c59d34f4318e5
n8fr8 <nathan@freitas.net>
2012-07-05 20:45:12 -0400

    added first code for calling android-ffmpeg-library

--- app/src/info/guardianproject/mrapp/MainActivity.java
+++ app/src/info/guardianproject/mrapp/MainActivity.java
@@ -1,10 +1,16 @@
 package info.guardianproject.mrapp;
 
-import android.os.Bundle;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.ffmpeg.android.FFMPEGWrapper;
+import org.ffmpeg.android.MediaDesc;
+import org.ffmpeg.android.ShellUtils.ShellCallback;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
 
@@ -20,5 +26,39 @@ public class MainActivity extends Activity {
         return true;
     }
 
+
+    public void testFFMPEG () throws FileNotFoundException, IOException, Exception
+    {
+    	FFMPEGWrapper ffmpegw = new FFMPEGWrapper (this);
+    	
+    	ArrayList<MediaDesc> mdlist = new ArrayList<MediaDesc>();
+    	
+    	MediaDesc mdesc = new MediaDesc ();
+    	mdesc.path = "somefile.mp4";
+    	mdesc.startTime = "00:00:03";
+    	mdesc.duration = 10;
+    	mdlist.add(mdesc);
+    	
+    	mdesc = new MediaDesc ();
+    	mdesc.path = "someotherfile.mp4";
+    	mdesc.startTime = "00:00:00";
+    	mdesc.duration = 5;
+    	mdlist.add(mdesc);
+    	
+    	MediaDesc mdout = new MediaDesc ();
+    	mdesc.path = "allthefilestogether.mp4";
+    	mdlist.add(mdesc);
+    	
+    	ffmpegw.concatAndTrimFiles(mdlist, mdout, new ShellCallback() {
+
+			@Override
+			public void shellOut(char[] msg) {
+				
+				System.out.println(msg);
+				
+			}
+    	});
+    
+    }
     
 }
