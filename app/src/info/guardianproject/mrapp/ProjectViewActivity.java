c49dd68f16893ba32846e285687cf415775647c5
n8fr8 <nathan@freitas.net>
2012-09-20 19:33:15 +0530

    media MPEG pre-convert; small UI updates;

--- app/src/info/guardianproject/mrapp/ProjectViewActivity.java
+++ app/src/info/guardianproject/mrapp/ProjectViewActivity.java
@@ -49,6 +49,7 @@ public class ProjectViewActivity extends SherlockActivity {
 	
 	private File mRenderPath;	
 	private File mMediaTmp;
+	private MediaDesc mMediaDescTmp;
 	
 	private MediaHelper mMediaHelper;
 	private MediaHelper.MediaResult mMediaResult;
@@ -138,17 +139,94 @@ public class ProjectViewActivity extends SherlockActivity {
 	    		Message msg = mHandler.obtainMessage(0);
 		         mHandler.sendMessage(msg);
 	    		
-	    		MediaScannerConnection.scanFile(
-	     				ProjectViewActivity.this,
-	     				new String[] {mRenderPath.getAbsolutePath()},
-	     				new String[] {MediaConstants.MIME_TYPE_MP4},
-	     				null);
+		         if (mRenderPath.exists() && mRenderPath.length() > 0)
+		         {
+		    		MediaScannerConnection.scanFile(
+		     				ProjectViewActivity.this,
+		     				new String[] {mRenderPath.getAbsolutePath()},
+		     				new String[] {MediaConstants.MIME_TYPE_MP4},
+		     				null);
+		    
+		    		msg = mHandler.obtainMessage(4);
+		            msg.getData().putString("path",mRenderPath.getAbsolutePath());
+		            
+		            mHandler.sendMessage(msg);
+		         }
+		         else
+		         {
+		        		msg = mHandler.obtainMessage(0);
+			            msg.getData().putString("status","Something went wrong with media export");
+
+				        mHandler.sendMessage(msg);
+				         
+		         }
+	    	}
+	    	catch (Exception e)
+	    	{
+	    		Message msg = mHandler.obtainMessage(0);
+	            msg.getData().putString("status","error: " + e.getMessage());
+
+		         mHandler.sendMessage(msg);
+	    		Log.e(AppConstants.TAG, "error exporting",e);
+	    	}
+		}
+	};
+	
+	 private void doPreconvertMedia (MediaDesc mediaIn)
+    {
+		mMediaDescTmp = mediaIn;
+		
+    	progressDialog = new ProgressDialog(this);
+    	progressDialog.setMessage("Importing Media. Please wait...");
+    	progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+    	progressDialog.setMax(100);
+        progressDialog.setCancelable(true);
+       
+    	 Message msg = mHandler.obtainMessage(0);
+         msg.getData().putString("status","cancelled");
+         progressDialog.setCancelMessage(msg);
+    	
+         progressDialog.show();
+     	
+		// Convert to video
+		Thread thread = new Thread (runConvertVideo);
+		thread.setPriority(Thread.MAX_PRIORITY);
+		thread.start();
+    
+    }
+	
+	Runnable runConvertVideo = new Runnable () {
+		
+		public void run ()
+		{
+	    	try
+	    	{
+	    	
+	    		MediaDesc mediaOut = convertMediaToMPEG(mMediaDescTmp);
+	    		File fileMediaOut = new File(mediaOut.path);
 	    		
+	    		Message msg = mHandler.obtainMessage(0);
+		         mHandler.sendMessage(msg);
 	    		
-	    		msg = mHandler.obtainMessage(4);
-	            msg.getData().putString("path",mRenderPath.getAbsolutePath());
-	            
-	            mHandler.sendMessage(msg);
+		         if (fileMediaOut.exists() && fileMediaOut.length() > 0)
+		         {
+		    		MediaScannerConnection.scanFile(
+		     				ProjectViewActivity.this,
+		     				new String[] {fileMediaOut.getAbsolutePath()},
+		     				new String[] {MediaConstants.MIME_TYPE_MPEG},
+		     				null);
+		    		
+		    		mMediaDescTmp.path = fileMediaOut.getAbsolutePath();
+		    		mMediaDescTmp.mimeType = MediaConstants.MIME_TYPE_MPEG;
+		    
+		         }
+		         else
+		         {
+		        	 msg = mHandler.obtainMessage(0);
+			            msg.getData().putString("status","Something went wrong with media import");
+
+				        mHandler.sendMessage(msg);
+				 }
 	    	}
 	    	catch (Exception e)
 	    	{
@@ -190,9 +268,11 @@ public class ProjectViewActivity extends SherlockActivity {
     //	DrawBoxVideoFilter vf = new DrawBoxVideoFilter(0,400,720,80,"red");
     //	mdout.videoFilter = vf.toString();
     	
+    	boolean mediaNeedConvert = false;
+    	
     	FfmpegController ffmpegc = new FfmpegController (this);
     	
-    	ffmpegc.concatAndTrimFiles(mediaList, mdout, new ShellCallback() {
+    	ffmpegc.concatAndTrimFiles(mediaList, mdout, mediaNeedConvert, new ShellCallback() {
 
 			@Override
 			public void shellOut(String line) {
@@ -248,6 +328,72 @@ public class ProjectViewActivity extends SherlockActivity {
     
    }
     
+    private MediaDesc convertMediaToMPEG (MediaDesc mediaIn) throws Exception
+    {
+    	
+    //	DrawBoxVideoFilter vf = new DrawBoxVideoFilter(0,400,720,80,"red");
+    //	mdout.videoFilter = vf.toString();
+    	
+    	FfmpegController ffmpegc = new FfmpegController (this);
+    	
+    	MediaDesc mediaOut = ffmpegc.convertToMPEG(mediaIn, new ShellCallback() {
+
+			@Override
+			public void shellOut(String line) {
+				
+				
+				if (!line.startsWith("frame"))
+					Log.d(AppConstants.TAG, line);
+				
+				
+				int idx1;
+				String newStatus = null;
+				int progress = 0;
+				
+				if ((idx1 = line.indexOf("Duration:"))!=-1)
+				{
+					int idx2 = line.indexOf(",", idx1);
+					String time = line.substring(idx1+10,idx2);
+					
+					int hour = Integer.parseInt(time.substring(0,2));
+					int min = Integer.parseInt(time.substring(3,5));
+					int sec = Integer.parseInt(time.substring(6,8));
+					
+					total = (hour * 60 * 60) + (min * 60) + sec;
+					
+					newStatus = line;
+					progress = 0;
+				}
+				else if ((idx1 = line.indexOf("time="))!=-1)
+				{
+					int idx2 = line.indexOf(" ", idx1);
+					String time = line.substring(idx1+5,idx2);
+					newStatus = line;
+					
+					int hour = Integer.parseInt(time.substring(0,2));
+					int min = Integer.parseInt(time.substring(3,5));
+					int sec = Integer.parseInt(time.substring(6,8));
+					
+					current = (hour * 60 * 60) + (min * 60) + sec;
+					
+					progress = (int)( ((float)current) / ((float)total) *100f );
+				}
+				
+				if (newStatus != null)
+				{
+				 Message msg = mHandler.obtainMessage(1);
+		         msg.getData().putInt("progress", progress);
+		         msg.getData().putString("status", newStatus);		         
+		         mHandler.sendMessage(msg);
+				}
+			}
+    	});
+    
+    	return mediaOut;
+    
+   }
+    
+    
     
     private void addMediaFile (String path, String mimeType)
     {
@@ -259,6 +405,15 @@ public class ProjectViewActivity extends SherlockActivity {
     	int mediaId = mediaList.size()-1;
     	
     	addMediaView(mdesc, mediaId);
+    	
+    	
+    	try {
+    		doPreconvertMedia(mdesc);
+		} catch (Exception e) {
+			Toast.makeText(this,"error converting video to mpeg",Toast.LENGTH_SHORT).show();
+			Log.e(AppConstants.TAG,"error converting video to mpeg",e);
+		}
+	
     	
     }
     
