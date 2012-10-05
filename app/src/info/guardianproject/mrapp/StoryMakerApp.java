03366486f85fec917b5c970027f9cc2527e0caf1
Josh Steiner <josh@vitriolix.com>
2012-10-05 11:20:53 -0700

    cleanup StoryMakerApp

--- app/src/info/guardianproject/mrapp/StoryMakerApp.java
+++ app/src/info/guardianproject/mrapp/StoryMakerApp.java
@@ -1,5 +1,8 @@
 package info.guardianproject.mrapp;
 
+import info.guardianproject.mrapp.db.StoryMakerDB;
+import info.guardianproject.mrapp.db.StoryMakerDB.Schema;
+
 import java.io.File;
 
 import net.sqlcipher.database.SQLiteDatabase;
@@ -7,21 +10,24 @@ import android.app.Application;
 
 public class StoryMakerApp extends Application {
 
-	
-	 public void InitializeSQLCipher(String dbName, String passphrase) {
-	        
-	      
-		 File databaseFile = getDatabasePath(dbName);
-	     databaseFile.mkdirs();
-	     SQLiteDatabase database = SQLiteDatabase.openOrCreateDatabase(databaseFile, passphrase, null);
-
-	  }
+//	public void InitializeSQLCipher(String dbName, String passphrase) {
+//
+//		File databaseFile = getDatabasePath(dbName);
+//		databaseFile.mkdirs();
+//		databaseFile.delete();
+//		SQLiteDatabase database = SQLiteDatabase.openOrCreateDatabase(databaseFile, passphrase, null);
+//		database.execSQL(StoryMakerDB.Schema.Projects.CREATE_TABLE_PROJECTS);
+//		database.execSQL(StoryMakerDB.Schema.Lessons.CREATE_TABLE_LESSONS);
+//		database.execSQL(StoryMakerDB.Schema.Medias.CREATE_TABLE_MEDIAS);
+//		database.close();
+//
+//	}
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		
-		 SQLiteDatabase.loadLibs(this);
 
+		SQLiteDatabase.loadLibs(this);
+//		InitializeSQLCipher("test.db", "");
 	}
 }
