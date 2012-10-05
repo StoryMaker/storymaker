d78dba7f7ff25f00cf68eb245c10970d590034a4
Josh Steiner <josh@vitriolix.com>
2012-10-05 10:35:04 -0700

    add save() method to Project DAO

--- app/src/info/guardianproject/mrapp/model/Project.java
+++ app/src/info/guardianproject/mrapp/model/Project.java
@@ -1,26 +1,35 @@
 package info.guardianproject.mrapp.model;
 
+import info.guardianproject.mrapp.db.ProjectsProvider;
 import info.guardianproject.mrapp.db.StoryMakerDB;
+import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 
 public class Project {
 
 	protected int id;
 	protected String title;
 	protected String thumbnailPath;
+	Context context;
 	
-	public Project() {
-		
+	public Project(Context context) {
+		this.context = context; 
 	}
 	
-	public Project(Cursor cursor) {
-		id = cursor.getInt(cursor.getColumnIndex(StoryMakerDB.Schema.Projects.ID)); // FIXME use indexes directly to opt
-		thumbnailPath = cursor.getString(cursor.getColumnIndex(StoryMakerDB.Schema.Projects.COL_THUMBNAIL_PATH)); // FIXME use indexes directly to opt
-		title = cursor.getString(cursor.getColumnIndex(StoryMakerDB.Schema.Projects.COL_TITLE)); // FIXME use indexes directly to opt
+	public Project(Context context, Cursor cursor) {
+		this.context = context;
+		id = cursor.getInt(cursor.getColumnIndex(StoryMakerDB.Schema.Projects.ID)); // FIXME use indexes directly to optimize this later
+		thumbnailPath = cursor.getString(cursor.getColumnIndex(StoryMakerDB.Schema.Projects.COL_THUMBNAIL_PATH)); // FIXME use indexes directly to optimize this later
+		title = cursor.getString(cursor.getColumnIndex(StoryMakerDB.Schema.Projects.COL_TITLE)); // FIXME use indexes directly to optimize this later
 	}
 	
 	public void save() {
-		
+		ContentValues values = new ContentValues();
+		values.put(StoryMakerDB.Schema.Projects.COL_TITLE, title);
+		values.put(StoryMakerDB.Schema.Projects.COL_THUMBNAIL_PATH, thumbnailPath);
+		context.getContentResolver().insert(ProjectsProvider.CONTENT_URI, values);
 	}
 	
 	/**
