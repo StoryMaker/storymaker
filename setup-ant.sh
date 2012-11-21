a37d544b9799978ab21df32fd0dc6bfdf7b927be
Abel Luck <abel@guardianproject.info>
2012-11-21 06:49:55 -0500

    Improve build setup
    * all dependencies are now submodules under external/
       * this is important so we're working off the same dependency versions
    * README has complete setup instructions
    * CLI builds with ant are easy to do

---
+++ setup-ant.sh
@@ -0,0 +1,19 @@
+
+if ! type -P android &> /dev/null; then
+    echo "Error: 'android' utility is not in your path."
+    echo "  Did you forget to setup the SDK?"
+    exit 1
+fi
+readarray <<END
+external/HoloEverywhere/contrib/ActionBarSherlock/library
+external/HoloEverywhere/library
+external/android-ffmpeg-java
+external/OnionKit/library
+external/WordpressJavaAndroid
+END
+
+for project in "${MAPFILE[@]}"; do
+        android update project --path $project
+done
+
+
