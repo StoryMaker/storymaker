deef5cdd60ffe1a7c4e159faef1911a6aee158a6
n8fr8 <nathan@freitas.net>
2012-12-05 12:20:10 +0530

    fixes to support Arabic lesson rendering better

--- app/assets/template/js/smtemplate.js
+++ app/assets/template/js/smtemplate.js
@@ -50,9 +50,9 @@ $.ajax({
  		$('#page' + i + ' a').attr("rel", "external");
 		$('#page' + i).trigger("create");
 		
-		$("#swmain").append('<div id="pagec" class="subswipe"><p></p></div>');
-		$('#pagec a').attr("rel", "external");
-		$('#pagec').trigger("create");
+		//$("#swmain").append('<div id="pagec" class="subswipe"><p></p></div>');
+		//$('#pagec a').attr("rel", "external");
+		//$('#pagec').trigger("create");
 	}
 	else
 	{
