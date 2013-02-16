02e00862650362216c5ed49f751d60860d4969f9
Josh Steiner <josh@vitriolix.com>
2013-02-15 17:37:41 -0800

    fix bug where AddClipsFragment was treating mTemplate.getScenes() if they were clips.  --> this might be the cause of the export bug

--- app/src/info/guardianproject/mrapp/AddClipsFragment.java
+++ app/src/info/guardianproject/mrapp/AddClipsFragment.java
@@ -83,14 +83,15 @@ public class AddClipsFragment extends Fragment {
         mAddClipsViewPager.setCurrentItem(cItemIdx);
     }
     
+    // only gets called from addShotToScene
     public void addTemplateClip (Clip clip) throws IOException, JSONException
     {
         mTemplate.getScene(0).addClip(clip); // FIXME get rid of hard code 0, should have a scene object directly
         mAddClipsPagerAdapter = new AddClipsPagerAdapter(mFm, mTemplate);
         mAddClipsViewPager.setAdapter(mAddClipsPagerAdapter);
         
-        mAddClipsViewPager.setCurrentItem(mTemplate.getScenes().size()-1);
-        mActivity.mMPM.mClipIndex = mTemplate.getScenes().size()-1;
+        mAddClipsViewPager.setCurrentItem(mTemplate.getScene(0).getClips().size()-1); // FIXME get rid of hard coded 0
+        mActivity.mMPM.mClipIndex = mTemplate.getScene(0).getClips().size()-1; // FIXME get rid of hard coded 0
         
         mActivity.mdExported = null;
         
@@ -124,7 +125,7 @@ public class AddClipsFragment extends Fragment {
                 @Override
                 public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
                     
-                    if (((position+1) == mTemplate.getScenes().size()) && positionOffset == 0 & positionOffsetPixels == 0)
+                    if (((position+1) == mTemplate.getScene(0).getClips().size()) && positionOffset == 0 & positionOffsetPixels == 0) // FIXME get rid of hard coded 0
                     {
                         mDragAtEnd++;
                         
@@ -195,7 +196,7 @@ public class AddClipsFragment extends Fragment {
         
         @Override
         public int getCount() {
-            return sTemplate.getScenes().size();
+            return sTemplate.getScene(0).getClips().size(); // FIXME get rid of hard coded 0
         }
 
         @Override
