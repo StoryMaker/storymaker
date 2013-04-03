d0ffc93124e3c0db0265d5faa80e6fc6a3fffe21
Josh Steiner <josh@vitriolix.com>
2013-04-02 17:46:56 -0700

    add Utils.convertTimeStringToInt

--- app/src/info/guardianproject/mrapp/Utils.java
+++ app/src/info/guardianproject/mrapp/Utils.java
@@ -1,5 +1,8 @@
 package info.guardianproject.mrapp;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import org.holoeverywhere.app.Activity;
 import org.holoeverywhere.widget.Toast;
 
@@ -12,5 +15,20 @@ public class Utils {
                         Toast.makeText(_activity.getApplicationContext(), _msg, Toast.LENGTH_SHORT).show();
                 }
         });
+    }
+    
+    /**
+     * 
+     * @param timeString in 00:00:00.000 format (hour:min:second.ms)
+     * @return time in ms
+     */
+    public static int convertTimeStringToInt(String timeString) {
+        int duration = 0;
+        try {
+            duration = (int) (new SimpleDateFormat("hh:mm:ss.SS").parse(timeString)).getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return duration;
     }
 }
