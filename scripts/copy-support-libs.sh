4e5d733e3810f50bccacfc8013f178a5a31bf545
Josh Steiner <josh@vitriolix.com>
2014-01-16 16:24:21 -0800

    adding SecureShareLib to scripts

--- scripts/copy-support-libs.sh
+++ scripts/copy-support-libs.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+echo "updating SlidingMenu" 
 cp app/libs/android-support-v4.jar external/SlidingMenu/library/libs/android-support-v4.jar
+
+echo "updating HoloEverywhere" 
 cp app/libs/android-support-v4.jar external/HoloEverywhere/library/libs/android-support-v4.jar
+
+echo "updating HoloEverywhere/contrib/ActionBarSherlock" 
 cp app/libs/android-support-v4.jar external/HoloEverywhere/contrib/ActionBarSherlock/library/libs/android-support-v4.jar
+
+echo "updating Android-ViewPagerIndicator" 
 cp app/libs/android-support-v4.jar external/Android-ViewPagerIndicator/library/libs/android-support-v4.jar
+
+echo "updating RangeSeekBar" 
 cp app/libs/android-support-v4.jar external/RangeSeekBar/library/libs/android-support-v4.jar
+
+echo "updating SecureShareLib" 
+cp app/libs/android-support-v4.jar external/SecureShareLib/SecureShareUILibrary/libs/android-support-v4.jar
