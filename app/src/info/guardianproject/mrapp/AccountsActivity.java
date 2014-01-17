537e3651f52a6e02927a0cada547700bf088eb23
Josh Steiner <josh@vitriolix.com>
2014-01-17 15:40:44 -0800

    adding missing files from accounts int.

---
+++ app/src/info/guardianproject/mrapp/AccountsActivity.java
@@ -0,0 +1,36 @@
+
+package info.guardianproject.mrapp;
+
+import io.scal.secureshareui.lib.ChooseAccountFragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+public class AccountsActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accounts);
+        
+        addChooseAccountFragment();
+    }
+    
+    public void addChooseAccountFragment() {
+        FragmentManager fragManager = getSupportFragmentManager();
+        FragmentTransaction fragTrans = fragManager.beginTransaction();
+             
+        Fragment caFragment = (Fragment) new ChooseAccountFragment();
+        fragTrans.add(R.id.fragmentLayout, caFragment);
+        fragTrans.commit();      
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.accounts, menu);
+//        return true;
+//    }
+
+}
