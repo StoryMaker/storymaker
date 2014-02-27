e2d66032f09b9cd6afd5a10adf1356dbd74f4e7e
David Brodsky <dbro@dbro.pro>
2014-02-27 01:10:02 -0800

    use Android Java style

--- app/src/info/guardianproject/mrapp/model/json/ProjectJson.java
+++ app/src/info/guardianproject/mrapp/model/json/ProjectJson.java
@@ -1,3 +1,4 @@
+
 package info.guardianproject.mrapp.model.json;
 
 import java.lang.reflect.Type;
@@ -19,100 +20,135 @@ import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
+/**
+ * Project Json Serialization and Deserialization
+ * @author David Brodsky
+ *
+ */
 public class ProjectJson {
-	
-	private static Gson mGson;
 
-	/***** Public API *****/
-	
-	public static JsonElement serializeAsObject(Context context, Project project){
-		return getGson(context).toJsonTree(project);
-	}
-	
-	public static String serializeAsString(Context context, Project project){
-		return getGson(context).toJson(project);
-	}
-	
-	public static Project deserialize(Context context, JsonElement projectJson){
-		return getGson(context).fromJson(projectJson, Project.class);
-	}
-	
-	/***** Private components *****/
+    private static Gson mGson;
 
-	private static Gson getGson(Context context){
-		if(mGson == null){
-			mGson = new GsonBuilder()
-						.registerTypeAdapter(Project.class, new ProjectSerializerDeserializer(context))
-						.registerTypeAdapter(Scene.class, new SceneJson.SceneSerializerDeserializer(context))
-						.registerTypeAdapter(Media.class, new MediaJson.MediaSerializerDeserializer(context))
-						.create();
-		}
-		return mGson;
-	}
+    /***** Public API *****/
 
-	/**
-	 * Serialize a Project into JSON.
-	 * Requires the Gson context to have registered type adapters
-	 * for Media and Scene serialization
-	 */
-	private static class ProjectSerializerDeserializer implements JsonSerializer<Project>, JsonDeserializer<Project> {
-		
-		private Context mContext;
-		
-		public ProjectSerializerDeserializer(Context context){
-			mContext = context;
-		}
-		
-		private Context getContext(){
-			return mContext;
-		}
-		 
-	    public JsonElement serialize(final Project project, final Type type, final JsonSerializationContext context) {
-	        JsonObject result = new JsonObject();
-	        result.add("id", new JsonPrimitive(project.getId()));
-	        result.add("storyType", new JsonPrimitive(project.getStoryType()));
-	        
-	        if(project.getTitle() != null)
-	        	result.add("title", new JsonPrimitive(project.getTitle()));
-	        if(project.getThumbnailPath() != null)
-	        	result.add("thumbnailPath", new JsonPrimitive(project.getThumbnailPath()));
-	        if(project.getTemplatePath() != null)
-	        	result.add("templatePath", new JsonPrimitive(project.getTemplatePath()));
-	        
-	        // Each Scene will serialize its media
-	        JsonArray sceneArray = new JsonArray();
-	        for(Scene scene : project.getScenesAsList()){
-	        	sceneArray.add(context.serialize(scene));
-	        }
-	        result.add("scenes", sceneArray);
-	        
-	        return result;
-	    }
+    /**
+     * Construct a JsonElement serialization of the given Project
+     * @param context an Android Application Context
+     * @param project the Project to serialize
+     * @return a JsonElement serialization of project
+     */
+    public static JsonElement serializeAsObject(Context context, Project project) {
+        return getGson(context).toJsonTree(project);
+    }
 
-		public Project deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-		      throws JsonParseException {
-			  JsonObject source = json.getAsJsonObject();
-			  JsonArray scenes = source.getAsJsonArray("scenes");
-			  
-			  Project project = new Project(mContext, (scenes == null) ? 0 : scenes.size());
-			  project.setId(source.getAsJsonPrimitive("id").getAsInt());
-			  project.setStoryType(source.getAsJsonPrimitive("storyType").getAsInt());
-			  
-			  if(source.has("title"))
-				  project.setTitle(source.getAsJsonPrimitive("title").getAsString());
-			  if(source.has("thumbnailPath"))
-				  project.setThumbnailPath(source.getAsJsonPrimitive("thumbnailPath").getAsString());
-			  if(source.has("templatePath"))
-				  project.setTemplatePath(source.getAsJsonPrimitive("templatePath").getAsString());
-			  
-			  if(scenes != null){
-				  for(int x = 0; x < scenes.size(); x++){
-					  project.setScene(0, (Scene) context.deserialize(scenes.get(x), Scene.class));
-				  }
-			  }
+    /**
+     * Construct a Json String serialization of the given Project
+     * @param context an Android Application Context
+     * @param project the Project to convert
+     * @return a Json String serialization of project
+     */
+    public static String serializeAsString(Context context, Project project) {
+        return getGson(context).toJson(project);
+    }
 
-			  return project;
-		}
-	}
+    /**
+     * Construct a Project from the given JsonElement
+     * @param context an Android Application Context
+     * @param projectJson the JsonElement serialization of the desired Project
+     * @return the Project deserialization of projectJson
+     */
+    public static Project deserialize(Context context, JsonElement projectJson) {
+        return getGson(context).fromJson(projectJson, Project.class);
+    }
+
+    /***** Private components *****/
+
+    private static Gson getGson(Context context) {
+        if (mGson == null) {
+            mGson = new GsonBuilder()
+                    .registerTypeAdapter(Project.class,
+                            new ProjectSerializerDeserializer(context))
+                    .registerTypeAdapter(Scene.class,
+                            new SceneJson.SceneSerializerDeserializer(context))
+                    .registerTypeAdapter(Media.class,
+                            new MediaJson.MediaSerializerDeserializer(context))
+                    .create();
+        }
+        return mGson;
+    }
+
+    /**
+     * Serialize a Project into JSON. Requires the Gson context to have
+     * registered type adapters for Media and Scene serialization
+     */
+    private static class ProjectSerializerDeserializer implements
+            JsonSerializer<Project>, JsonDeserializer<Project> {
+
+        private Context mContext;
+
+        public ProjectSerializerDeserializer(Context context) {
+            mContext = context;
+        }
+
+        private Context getContext() {
+            return mContext;
+        }
+
+        public JsonElement serialize(final Project project, final Type type,
+                final JsonSerializationContext context) {
+            JsonObject result = new JsonObject();
+            result.add("id", new JsonPrimitive(project.getId()));
+            result.add("storyType", new JsonPrimitive(project.getStoryType()));
+
+            if (project.getTitle() != null) {
+                result.add("title", new JsonPrimitive(project.getTitle()));
+            }
+            if (project.getThumbnailPath() != null) {
+                result.add("thumbnailPath",
+                        new JsonPrimitive(project.getThumbnailPath()));
+            }
+            if (project.getTemplatePath() != null) {
+                result.add("templatePath",
+                        new JsonPrimitive(project.getTemplatePath()));
+            }
+
+            // Each Scene will serialize its media
+            JsonArray sceneArray = new JsonArray();
+            for (Scene scene : project.getScenesAsList()) {
+                sceneArray.add(context.serialize(scene));
+            }
+            result.add("scenes", sceneArray);
+
+            return result;
+        }
+
+        public Project deserialize(JsonElement json, Type typeOfT,
+                JsonDeserializationContext context) throws JsonParseException {
+            JsonObject source = json.getAsJsonObject();
+            JsonArray scenes = source.getAsJsonArray("scenes");
+
+            Project project = new Project(mContext, (scenes == null) ? 0 : scenes.size());
+            project.setId(source.getAsJsonPrimitive("id").getAsInt());
+            project.setStoryType(source.getAsJsonPrimitive("storyType").getAsInt());
+
+            if (source.has("title")) {
+                project.setTitle(source.getAsJsonPrimitive("title").getAsString());
+            }
+            if (source.has("thumbnailPath")) {
+                project.setThumbnailPath(source.getAsJsonPrimitive("thumbnailPath").getAsString());
+            }
+            if (source.has("templatePath")) {
+                project.setTemplatePath(source.getAsJsonPrimitive("templatePath").getAsString());
+            }
+
+            if (scenes != null) {
+                for (int x = 0; x < scenes.size(); x++) {
+                    project.setScene(0, (Scene) context.deserialize(scenes.get(x), Scene.class));
+                }
+            }
+
+            return project;
+        }
+    }
 
 }
--- app/src/info/guardianproject/mrapp/model/json/SceneJson.java
+++ app/src/info/guardianproject/mrapp/model/json/SceneJson.java
@@ -1,10 +1,5 @@
+
 package info.guardianproject.mrapp.model.json;
-
-import info.guardianproject.mrapp.model.Media;
-import info.guardianproject.mrapp.model.Project;
-import info.guardianproject.mrapp.model.Scene;
-
-import java.lang.reflect.Type;
 
 import android.content.Context;
 
@@ -18,67 +13,81 @@ import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
+import info.guardianproject.mrapp.model.Media;
+import info.guardianproject.mrapp.model.Scene;
+
+import java.lang.reflect.Type;
+
+/**
+ * Scene Json Serialization and Deserialization
+ * 
+ * @author David Brodsky
+ */
 public class SceneJson {
-	
-public static class SceneSerializerDeserializer implements JsonSerializer<Scene>, JsonDeserializer<Scene> {
-		
-		private Context mContext;
-		
-		public SceneSerializerDeserializer(Context context){
-			mContext = context;
-		}
-		
-		private Context getContext(){
-			return mContext;
-		}
-		 
-	    public JsonElement serialize(final Scene scene, final Type type, final JsonSerializationContext context) {
-	        JsonObject result = new JsonObject();
-	        result.add("id", new JsonPrimitive(scene.getId()));
-	        result.add("projectIndex", new JsonPrimitive(scene.getProjectIndex()));
-	        result.add("projectId", new JsonPrimitive(scene.getProjectId()));
-	        result.add("clipCount", new JsonPrimitive(scene.getClipCount()));
-	        
-	        if(scene.getTitle() != null)
-	        	result.add("title", new JsonPrimitive(scene.getTitle()));
-	        if(scene.getThumbnailPath() != null)
-	        	result.add("thumbnailPath", new JsonPrimitive(scene.getThumbnailPath()));
-	        
-	        
-	        JsonArray mediaArray = new JsonArray();
-	        for(Media media : scene.getMediaAsList()){
-	        	mediaArray.add(context.serialize(media));
-	        }
-	        result.add("media", mediaArray);
-	        
-	        return result;
-	    }
 
-		public Scene deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-		      throws JsonParseException {
-			  JsonObject source = json.getAsJsonObject();
-			  JsonArray media = source.getAsJsonArray("media");
-			  
-			  Scene scene = new Scene(mContext, (media == null) ? 0 : media.size());
-			  scene.setId(source.getAsJsonPrimitive("id").getAsInt());
-			  scene.setProjectIndex(source.getAsJsonPrimitive("projectIndex").getAsInt());
-			  scene.setProjectId(source.getAsJsonPrimitive("projectId").getAsInt());
-			  scene.setClipCount(source.getAsJsonPrimitive("clipCount").getAsInt());
-			  
-			  if(source.has("title"))
-				  scene.setTitle(source.getAsJsonPrimitive("title").getAsString());
-			  if(source.has("thumbnailPath"))
-				  scene.setThumbnailPath(source.getAsJsonPrimitive("thumbnailPath").getAsString());
-			  
+    public static class SceneSerializerDeserializer implements JsonSerializer<Scene>,
+            JsonDeserializer<Scene> {
 
-			  if(media != null){
-				  for(int x = 0; x < media.size(); x++){
-					  scene.setMedia(0, (Media) context.deserialize(media.get(x), Media.class));
-				  }
-			  }
+        private Context mContext;
 
-			  return scene;
-		}
-	}
+        public SceneSerializerDeserializer(Context context) {
+            mContext = context;
+        }
+
+        private Context getContext() {
+            return mContext;
+        }
+
+        public JsonElement serialize(final Scene scene, final Type type,
+                final JsonSerializationContext context) {
+            JsonObject result = new JsonObject();
+            result.add("id", new JsonPrimitive(scene.getId()));
+            result.add("projectIndex", new JsonPrimitive(scene.getProjectIndex()));
+            result.add("projectId", new JsonPrimitive(scene.getProjectId()));
+            result.add("clipCount", new JsonPrimitive(scene.getClipCount()));
+
+            if (scene.getTitle() != null) {
+                result.add("title", new JsonPrimitive(scene.getTitle()));
+            }
+            if (scene.getThumbnailPath() != null) {
+                result.add("thumbnailPath", new JsonPrimitive(scene.getThumbnailPath()));
+            }
+
+            JsonArray mediaArray = new JsonArray();
+            for (Media media : scene.getMediaAsList()) {
+                mediaArray.add(context.serialize(media));
+            }
+            result.add("media", mediaArray);
+
+            return result;
+        }
+
+        public Scene deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+                throws JsonParseException {
+            JsonObject source = json.getAsJsonObject();
+            JsonArray media = source.getAsJsonArray("media");
+
+            Scene scene = new Scene(mContext, (media == null) ? 0 : media.size());
+            scene.setId(source.getAsJsonPrimitive("id").getAsInt());
+            scene.setProjectIndex(source.getAsJsonPrimitive("projectIndex").getAsInt());
+            scene.setProjectId(source.getAsJsonPrimitive("projectId").getAsInt());
+            scene.setClipCount(source.getAsJsonPrimitive("clipCount").getAsInt());
+
+            if (source.has("title")) {
+                scene.setTitle(source.getAsJsonPrimitive("title").getAsString());
+            }
+            if (source.has("thumbnailPath")) {
+                scene.setThumbnailPath(source.getAsJsonPrimitive("thumbnailPath").getAsString());
+            }
+
+            if (media != null) {
+                for (int x = 0; x < media.size(); x++) {
+                    scene.setMedia(0, (Media) context.deserialize(media.get(x), Media.class));
+                }
+            }
+
+            return scene;
+        }
+    }
 
 }
