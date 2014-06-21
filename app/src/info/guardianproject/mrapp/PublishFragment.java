2fd85472749f146d1a47fc62a594adce7a5165f1
Micah Lucas <micah@scal.io>
2014-06-20 21:23:40 -0700

    First try at getting smart SM login/publish flow.

--- app/src/info/guardianproject/mrapp/PublishFragment.java
+++ app/src/info/guardianproject/mrapp/PublishFragment.java
@@ -80,6 +80,8 @@ public class PublishFragment extends Fragment implements PublishListener {
     private final static String TAG = "PublishFragment";
     
     private final static int REQ_SOUNDCLOUD = 777;
+    private static Intent serviceIntent = null;
+    private static Activity attachedActivity = null;
     
     public ViewPager mAddClipsViewPager;
     View mView = null;
@@ -790,17 +792,6 @@ public class PublishFragment extends Fragment implements PublishListener {
                     boolean useTor = intent.getBooleanExtra(ChooseAccountFragment.EXTRAS_USE_TOR, false);
                     boolean publishToStoryMaker = intent.getBooleanExtra(ChooseAccountFragment.EXTRAS_PUBLISH_TO_STORYMAKER, false);
                     
-                    if(publishToStoryMaker) {
-                    	ServerManager sm = StoryMakerApp.getServerManager();
-                        sm.setContext(mActivity.getBaseContext());
-                        
-                        if(!sm.hasCreds()) {
-                        	Intent connectSMIntent = new Intent(mActivity, ConnectAccountActivity.class);
-                        	connectSMIntent.putExtra("isPublishPending", true); //TODO add logic to ConnectAccountActivity to return here and continue publish
-                        	getActivity().startActivity(connectSMIntent);
-                        }
-                    }
-
                     showUploadSpinner(true);
                     mUploading = true;
                     mPlaying = false;
@@ -829,7 +820,7 @@ public class PublishFragment extends Fragment implements PublishListener {
         i.putExtra(PublishService.INTENT_EXTRA_USE_TOR, useTor);
         i.putExtra(PublishService.INTENT_EXTRA_PUBLISH_TO_STORYMAKER, publishToStoryMaker);
         i.putExtra(PublishService.INTENT_EXTRA_SITE_KEYS, siteKeys);
-        getActivity().startService(i);
+        startService(i, publishToStoryMaker);
     }
     
     private void startUpload(Project project, String[] siteKeys, boolean useTor, boolean publishToStoryMaker) {
@@ -839,9 +830,32 @@ public class PublishFragment extends Fragment implements PublishListener {
         i.putExtra(PublishService.INTENT_EXTRA_USE_TOR, useTor);
         i.putExtra(PublishService.INTENT_EXTRA_PUBLISH_TO_STORYMAKER, publishToStoryMaker);
         i.putExtra(PublishService.INTENT_EXTRA_SITE_KEYS, siteKeys);
-        getActivity().startService(i);
+        startService(i, publishToStoryMaker);
     }
-	
+    
+    private void startService(Intent i, boolean publishToStoryMaker) {
+        if(publishToStoryMaker) {
+        	attachedActivity = getActivity();
+        	serviceIntent = i;
+        	
+        	ServerManager sm = StoryMakerApp.getServerManager();
+            sm.setContext(mActivity.getBaseContext());
+            
+            if(!sm.hasCreds()) {
+            	Intent connectSMIntent = new Intent(mActivity, ConnectAccountActivity.class);
+            	connectSMIntent.putExtra("isPublishPending", true);
+            	getActivity().startActivity(connectSMIntent);
+            }
+        }
+    	
+    	getActivity().startService(i);
+    }
+    
+    public void continuePublishService() {	
+    	if(null != serviceIntent) {
+    		attachedActivity.startService(serviceIntent);
+    	}    	
+    }
 
     @Override
     public void publishSucceeded(PublishJob publishJob, Job job) {
