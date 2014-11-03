edf5cb6a860f3a3d5a22d99db55feb29db30b0fb
Josh Steiner <josh@vitriolix.com>
2014-11-02 21:01:26 -0800

    updating content script

--- content.py
+++ content.py
@@ -9,37 +9,51 @@ def cli():
 
     
 @cli.command()    
-def setup(foo):
+def clone():
+    """clone the liger content git repo"""
+
     os.system('git clone https://github.com/scalio/liger-content.git')
 
 @cli.command()    
-def update():
+def pull():
+    """update the liger content repo"""
+
     os.system('cd liger-content ; git pull')
-    
+
+@cli.command()
+def generate_json():
+    """generate json from yaml, also splits out strings into translation intermediates ready for pushing"""
+
+    os.system("cd liger-content ; python generate_content.py")
+
 @cli.command()    
 def push_strings():
-    pass
-    
-@cli.command()    
-def update_translations(foo):
-    os.system('cd liger-content ; tx pull')
+    """First generates the content to extract the latest source strings, then pushes them to transifex"""
+    cli.echo("\n\nupdateing content...\n\n")
+    os.system("cd liger-content ; python generate_content.py")
+    cli.echo("\n\npushing strings to transifex\n\n")
+    os.system('cd liger-content ; tx push -s')
+
+@cli.command()
+def update_strings(foo):
+    """pull down translated strings from transifex and generate localized json"""
+    cli.echo("\n\npulling translations from transifex...\n\n")
+    os.system('cd liger-content ; pull_translations.py')
+
+    cli.echo("\n\ngenerating localized content...\n\n")
     os.system('cd liger-content ; python generate_localized_content.py ')
    
 @cli.command()  
-def generate_content():
+def zip_content():
+    """this creates the zipped blob of content and copies it in to storymaker's assets folder as its .obb file"""
     os.system("mkdir liger-content/assets ; cd liger-content/assets ; zip -n .mp4 -r zipped . ; mv zipped.zip ../../app/assets/main.1.obb")
 
 
-@cli.command()
-def build():
-    print "building content zip..."
 
-
-
-cli.add_command(setup)
-cli.add_command(update)
-cli.add_command(build)
-cli.add_command(update_translations)
-cli.add_command(generate_content)
+cli.add_command(clone)
+cli.add_command(pull)
+cli.add_command(push_strings)
+cli.add_command(update_strings)
+cli.add_command(zip_content)
 
 cli()
