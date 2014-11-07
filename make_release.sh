6d0f2b2c1ecee43d24060524b269a7e7d98d6307
Josh Steiner <josh@vitriolix.com>
2014-11-06 19:06:23 -0800

    add simple script to help automate making release build, release keys handled elseware and place in home dir as storymaker-release.key

---
+++ make_release.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+./gradlew assembleRelease
+
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore storymaker-release.key app/build/outputs/apk/app-release-unsigned.apk release
+
+jarsigner -verify -verbose -certs app/build/outputs/apk/app-release-unsigned.apk
+
+/home/josh/android-sdks/build-tools/21.0.0/zipalign -v 4 app/build/outputs/apk/app-release-unsigned.apk storymaker-release.apk
