fb55739a6245672988d911b8199e0a49a79a1206
Josh Steiner <josh@vitriolix.com>
2014-12-19 19:29:37 -0800

    restructure gradle and project src folders so we can have two product flavors, one with sqlite and one with sqlicipher so we can test agaisnt sqlite until espresso plays nice with sqlcipher

---
+++ app/mainSqlite/java/org/storymaker/app/model/LessonTable.java
@@ -0,0 +1,38 @@
+package org.storymaker.app.model;
+
+import android.database.sqlite.SQLiteDatabase;
+import org.storymaker.app.db.ProjectsProvider;
+import org.storymaker.app.db.StoryMakerDB;
+import android.net.Uri;
+
+public class LessonTable extends Table {
+    private final static String TAG = "LessonTable";
+    
+    public LessonTable() {
+        
+    }
+    
+    public LessonTable(SQLiteDatabase db) {
+        super(db);
+    }
+
+    @Override
+    protected String getTableName() {
+        return StoryMakerDB.Schema.Lessons.NAME;
+    }
+    
+    @Override
+    protected String getIDColumnName() {
+        return StoryMakerDB.Schema.Lessons.ID;
+    }
+
+    @Override
+    protected Uri getURI() {
+        return ProjectsProvider.LESSONS_CONTENT_URI;
+    }
+
+    @Override
+    protected String getProviderBasePath() {
+        return ProjectsProvider.LESSONS_BASE_PATH;
+    }
+}
---
+++ app/mainSqlite/java/org/storymaker/app/model/PublishJobTable.java
@@ -0,0 +1,96 @@
+package org.storymaker.app.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import android.database.sqlite.SQLiteDatabase;
+import org.storymaker.app.Utils;
+import org.storymaker.app.db.ProjectsProvider;
+import org.storymaker.app.db.StoryMakerDB;
+import android.content.Context;
+import android.net.Uri;
+
+
+/**
+ * 
+ * @author Josh Steiner <josh@vitriolix.com>
+ *
+ */
+public class PublishJobTable extends Table {
+    private final static String TAG = "PublishJobTable";
+    
+
+    public PublishJobTable() {
+        
+    }
+    
+    public PublishJobTable(SQLiteDatabase db) {
+        super(db);
+    }
+    
+    @Override            
+    protected String getTableName() {
+        return StoryMakerDB.Schema.PublishJobs.NAME;
+    }
+    
+    @Override
+    protected String getIDColumnName() {
+        return StoryMakerDB.Schema.PublishJobs.ID;
+    }
+
+    @Override
+    protected Uri getURI() {
+        return ProjectsProvider.PUBLISH_JOBS_CONTENT_URI;
+    }
+
+    @Override
+    protected String getProviderBasePath() {
+        return ProjectsProvider.PUBLISH_JOBS_BASE_PATH;
+    }
+    
+    // HELPERS //////
+    
+    public ArrayList<PublishJob> getUnfinishedAsList(Context context, int projectId, String[] siteKeys) {
+        // FIXME this isn't very optimized
+        // type can be TYPE_UPLOAD or TYPE_RENDER
+        ArrayList<PublishJob> jobs = (ArrayList<PublishJob>) getAllAsList(context); // TODO need to make this method for reals
+        ArrayList<PublishJob> purgedList = null;
+        if (jobs != null) {
+            for (PublishJob job: jobs) {
+                if ((job.finishedAt == null) // FIXME this should be handled in SQL not here
+                        && job.projectId == projectId
+                        && keysMatch(job.siteKeys, siteKeys)) { // FIXME this siteKeys check is fragile, we should sort the list first
+                    if (purgedList == null) {
+                        purgedList = new ArrayList<PublishJob>();
+                    }
+                    purgedList.add(job);
+                }
+            }
+        }
+        return purgedList;
+    }
+    
+    // FIXME kaybe we need to trim the arrays to weed out dupes?
+    private boolean keysMatch(String[] keys1, String[] keys2) {
+        if (keys1 == null && keys2 == null) {
+            return true;
+        } else if (keys1 == null || keys2 == null) {
+            return false;
+        }
+        Arrays.sort(keys1);
+        Arrays.sort(keys2);
+        String keyString1 = Utils.stringArrayToCommaString(keys1);
+        String keyString2 = Utils.stringArrayToCommaString(keys2);
+        return keyString1.equals(keyString2);
+    }
+    
+    // job's are only ready to be run if they have a queuedAt time.  You can enter jobs in the table before they are ready to run
+    public PublishJob getNextUnfinished(Context context, int projectId, String[] siteKeys) {
+        ArrayList<PublishJob> jobs = getUnfinishedAsList(context, projectId, siteKeys);
+        if (jobs != null) {
+            return jobs.get(0); // FIXME is the 0th really the next in line ?  is the list sorted by id?
+        } else {
+            return null;
+        }
+    }
+}
