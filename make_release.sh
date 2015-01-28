6b1d62829ae0c0c0bb8c19562f6be3425dce46cc
Josh Steiner <josh@vitriolix.com>
2015-01-28 10:07:34 -0800

    updating make_release to work with new build targets

--- make_release.sh
+++ make_release.sh
@@ -2,10 +2,10 @@
 
 # from:  http://developer.android.com/tools/publishing/app-signing.html
 
-./gradlew assembleRelease
+#./gradlew assembleRelease
 
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore storymaker-release.key app/build/outputs/apk/app-release-unsigned.apk release
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore storymaker-release.key app/build/outputs/apk/app-mainSqlCipher-release-unsigned.apk release
 
-jarsigner -verify -verbose -certs app/build/outputs/apk/app-release-unsigned.apk
+jarsigner -verify -verbose -certs app/build/outputs/apk/app-mainSqlCipher-release-unsigned.apk
 
-/home/josh/android-sdks/build-tools/21.0.0/zipalign -v 4 app/build/outputs/apk/app-release-unsigned.apk storymaker-release.apk
+/home/josh/Android/Sdk/build-tools/21.1.2/zipalign -v 4 app/build/outputs/apk/app-mainSqlCipher-release-unsigned.apk storymaker-release.apk
