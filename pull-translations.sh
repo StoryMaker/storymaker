d99418d1b601443896f8339026e835343c9eb855
Josh Steiner <josh@vitriolix.com>
2015-03-23 19:26:48 -0700

    update pull translations script

--- pull-translations.sh
+++ pull-translations.sh
@@ -8,5 +8,8 @@ mv app/assets/LessonTitles-bs_BA.txt app/assets/LessonTitles-bs.txt
 rm -r app/res/values-bs
 mv app/res/values-bs_BA app/res/values-bs
 
+rm -R external/liger/lib/src/main/res/values-bs
+mv external/liger/lib/src/main/res/values-bs_BA external/liger/lib/src/main/res/values-bs
+
 rm -r app/assets/story/templates/bs
 mv app/assets/story/templates/bs_BA app/assets/story/templates/bs
