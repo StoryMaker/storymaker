ae9f837f9d129e188229d6952c54f1c92e195460
Josh Steiner <josh@vitriolix.com>
2015-07-21 21:12:19 -0700

    revert to bare storymaker.org base url, adding api. programatically

--- app/src/org/storymaker/app/Constants.java
+++ app/src/org/storymaker/app/Constants.java
@@ -7,5 +7,5 @@ public class Constants {
     public static final String PREFERENCE_ANALYTICS_OPTIN = "analytics.optin";
     public static final String PREFERENCES_ANALYTICS = "analytics";
     public static final String PREFERENCES_WP_REGISTERED = "wp.registered";
-    public static final String DEFAULT_SERVER_URL = "https://api.storymaker.org/"; // FIXME we should not use api in here, but prepend it programatically.  we will need to migrate peoples settings if they have api. saved in prefs
+    public static final String DEFAULT_SERVER_URL = "https://storymaker.org/";
 }
