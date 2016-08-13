55c24dc89602e0308c0f5cc61440c1a71f34f875
n8fr8 <nathan@freitas.net>
2016-08-12 23:09:21 -0400

    redirect onActivityResult to the fragment for handling this allows new accounts to be setup again!

--- app/src/org/storymaker/app/AccountsActivity.java
+++ app/src/org/storymaker/app/AccountsActivity.java
@@ -122,6 +122,6 @@ public class AccountsActivity extends BaseActivity {
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		//caFragment.onActivityResult(requestCode, resultCode, data);
+		caFragment.onActivityResult(requestCode, resultCode, data);
 	} 
 }
